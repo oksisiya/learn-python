80310e24dabd96c65ccfeacd4c48c8f94cc9ce82
Suyeon Oh <125956492+oksisiya@users.noreply.github.com>
2023-05-09 16:55:20 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 11.73 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,3 +1,8 @@
 def solution(arr, queries):
     answer = []
-    return answer
+    
+    for i in range(len(queries)):
+        answer.append([j for j in arr[queries[i][0]:queries[i][1]+1] if j > queries[i][2]])
+        if len(answer[i]) == 0: answer[i].append(-1)
+        
+    return [min(i) for i in answer]
